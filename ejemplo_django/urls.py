568d0643671a8e29e20182e137d207aa8b24991d
alexander <fsegovia2497@gmail.com>
2022-08-31 20:32:08 -0500

    V1.2

--- ejemplo_django/urls.py
+++ ejemplo_django/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('hola',views.hola,name='hola'),
     path('hastaLuego',views.hastaLuego,name='hastaLuego'),
+    path('usuarioInfo',views.usuarioInfo,name='usuarioInfo'),
 ]
--- ejemplo_django/views.py
+++ ejemplo_django/views.py
@@ -2,6 +2,7 @@ from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
+informacion_usuarios = [['Alexander','Segovia'],['Javier','Hilario'],['Martin','Leyva']]
 # Create your views here.
 
 def index(request):
@@ -15,3 +16,15 @@ def hola(request):
 
 def hastaLuego(request): 
     return render(request,'ejemplo_django/hastaLuego.html')
+
+def usuarioInfo(request):
+    if request.method == 'POST':
+        nombreUsuario = request.POST.get('usuarioNombre')
+        apellidoUsuario = request.POST.get('usuarioApellido')
+        informacion_usuarios.append([nombreUsuario,apellidoUsuario])
+        return render(request,'ejemplo_django/usuariosInfo.html',{
+            'informacion_usuarios':informacion_usuarios,
+        })
+    return render(request,'ejemplo_django/usuariosInfo.html',{
+        'informacion_usuarios':informacion_usuarios,
+    })
